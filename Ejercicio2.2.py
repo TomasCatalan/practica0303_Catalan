46b09f79a5ec57486d25bf7ea8a56a18f04e36be
TomasCatalan <tcatalaota@educacion.navarra.es>
2024-01-30 13:53:08 +0100

    Update Ejercicio2.2.py

--- Ejercicio2.2.py
+++ Ejercicio2.2.py
@@ -16,26 +16,57 @@ speed = [4,4]
 # Pongo la pelota en el origen de coordenadas
 ballrect = ball.get_rect()
 
+# Lugar de inicio de la bola
+ballrect.move_ip(0,0)
+
+# Crea el objeto bate, y obtengo su rectángulo
+bate = pygame.image.load("nuke(barra).png")
+baterect = bate.get_rect()
+
+# Pongo el bate en la parte inferior de la pantalla
+baterect.move_ip(240,450)
+
+
 jugando = True
 while jugando:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             jugando = False
 
-    # Muevo la pelota
-    ballrect = ballrect.move(speed)
+    # Compruebo si se ha pulsado alguna tecla
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        baterect = baterect.move(-3,0)
+    if keys[pygame.K_RIGHT]:
+        baterect = baterect.move(3,0)
 
-    # Compruebo si la pelota llega a los límites de la ventana
-    if ballrect.left < 0 or ballrect.right > ventana.get_width():
-        speed[0] = -speed[0]
-            
-    if ballrect.top < 0 or ballrect.bottom > ventana.get_height():
+    # Compruebo si hay colisión
+    if baterect.colliderect(ballrect):
         speed[1] = -speed[1]
     
+    # Muevo la pelota
+    ballrect = ballrect.move(speed)
+    
+     # Compruebo si la pelota llega a los límites de la ventana
+    if ballrect.left < 0 or ballrect.right > ventana.get_width():
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > ventana.get_height():
+        speed[1] = -speed[1]
     ventana.fill((238, 255, 172))
+    ventana.blit(ball, ballrect)
+
+
+    
+   
+    
+    
 
     # Dibujo la pelota
     ventana.blit(ball, ballrect)
+    
+    # Dibujo el bate
+    ventana.blit(bate, baterect)
+
     pygame.display.flip()
     pygame.time.Clock().tick(60)
 
