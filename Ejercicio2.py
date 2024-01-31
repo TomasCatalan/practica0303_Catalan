48b117d30a479eef2639fb0e334bd6f8564b505b
jotano <jotanosanm@educacion.navarra.es>
2024-01-31 21:05:49 +0100

    .

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -5,37 +5,77 @@ ventana = pygame.display.set_mode((1080,720))
 pygame.display.set_caption("ejercicio 2")
 
 # Crea el objeto pelota
-ball = pygame.image.load("OBAMNA.png")
+ball = pygame.image.load("perro.png")
 
 # Transforma el tamaño del objeto ball
-ball = pygame.transform.scale(ball, (60, 60))
+ball = pygame.transform.scale(ball, (250, 140))
 
 # Inicializo los valores con los que se van a mover la pelota
-speed = [6,6]
+speed = [4,4]
 
 # Pongo la pelota en el origen de coordenadas
 ballrect = ball.get_rect()
 
+# Lugar de inicio de la bola
+ballrect.move_ip(0,0)
+
+# Crea el objeto bate, y obtengo su rectángulo
+bate = pygame.image.load("barra(perro).png")
+
+baterect = bate.get_rect()
+
+# Pongo el bate en la parte inferior de la pantalla
+baterect.move_ip(240,450)
+
+# Transforma el tamaño del objeto bate
+bate = pygame.transform.scale(bate, (150, 140))
+
+
+
+
+
+
+        
 jugando = True
 while jugando:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             jugando = False
 
-    # Muevo la pelota
-    ballrect = ballrect.move(speed)
+    # Compruebo si se ha pulsado alguna tecla
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        baterect = baterect.move(-3,0)
+    if keys[pygame.K_RIGHT]:
+        baterect = baterect.move(3,0)
 
-    # Compruebo si la pelota llega a los límites de la ventana
-    if ballrect.left < 0 or ballrect.right > ventana.get_width():
-        speed[0] = -speed[0]
-            
-    if ballrect.top < 0 or ballrect.bottom > ventana.get_height():
+    # Compruebo si hay colisión
+    if baterect.colliderect(ballrect):
         speed[1] = -speed[1]
     
-    ventana.fill((252, 243, 207))
+    # Muevo la pelota
+    ballrect = ballrect.move(speed)
+    
+     # Compruebo si la pelota llega a los límites de la ventana
+    if ballrect.left < 0 or ballrect.right > ventana.get_width():
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > ventana.get_height():
+        speed[1] = -speed[1]
+    ventana.fill((238, 255, 172))
+    ventana.blit(ball, ballrect)
+
+
+    
+   
+    
+    
 
     # Dibujo la pelota
     ventana.blit(ball, ballrect)
+    
+    # Dibujo el bate
+    ventana.blit(bate, baterect)
+
     pygame.display.flip()
     pygame.time.Clock().tick(60)
 
