2990bec474519cdb58bddff8a375d251e94e6071
TomasCatalan <tcatalaota@educacion.navarra.es>
2024-02-02 11:53:42 +0100

    Update Ejercicio2.2.py

--- Ejercicio2.2.py
+++ Ejercicio2.2.py
@@ -2,7 +2,7 @@ import pygame
 
 pygame.init()
 ventana = pygame.display.set_mode((1080,720))
-pygame.display.set_caption("ejercicio 2")
+pygame.display.set_caption("ejercicio 2.2")
 
 # Crea el objeto pelota
 ball = pygame.image.load("OBAMNA.png")
@@ -23,19 +23,8 @@ ballrect = ball.get_rect()
 # Lugar de inicio de la bola
 ballrect.move_ip(0,0)
 
-# Crea el objeto bate, y obtengo su rectángulo
-bate = pygame.image.load("nuke(barra)3.png")
-
-# Transforma el tamaño del objeto bate
-bate = pygame.transform.scale(bate, (200, 55))
-
 # Transforma el tamaño de la imagen de fondo
 fondo = pygame.transform.scale(fondo, (1080, 720))
-
-baterect = bate.get_rect()
-
-# Pongo el bate en la parte inferior de la pantalla
-baterect.move_ip(440,650)
 
 
 jugando = True
@@ -44,17 +33,6 @@ while jugando:
         if event.type == pygame.QUIT:
             jugando = False
 
-    # Compruebo si se ha pulsado alguna tecla, y establece la velocidad del bate
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        baterect = baterect.move(-10,0)
-    if keys[pygame.K_RIGHT]:
-        baterect = baterect.move(10,0)
-
-    # Compruebo si hay colisión
-    if baterect.colliderect(ballrect):
-        speedball[1] = -speedball[1]
-    
     # Muevo la pelota
     ballrect = ballrect.move(speedball)
     
@@ -64,12 +42,6 @@ while jugando:
     if ballrect.top < 0 or ballrect.bottom > ventana.get_height():
         speedball[1] = -speedball[1]
     
-    # Establece colision del bate con las paredes de la ventana
-    if baterect.left < 0:
-       baterect = baterect.move(10,0)
-    if baterect.right > ventana.get_width():
-        baterect = baterect.move(-10,0)
-    
     #establece el fondo como una imagen, fill evita trazado de la bola
     ventana.fill((0, 0, 0))
     ventana.blit(fondo, (0,0))
@@ -78,9 +50,6 @@ while jugando:
 
     # Dibujo la pelota
     ventana.blit(ball, ballrect)
-    
-    # Dibujo el bate
-    ventana.blit(bate, baterect)
 
     pygame.display.flip()
     pygame.time.Clock().tick(60)
