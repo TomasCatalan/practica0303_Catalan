8a6911de89d9ad45343ca94f2e2773cc290bdb39
TomasCatalan <tcatalaota@educacion.navarra.es>
2024-02-02 11:18:21 +0100

    Update Ejercicio2.2.py

--- Ejercicio2.2.py
+++ Ejercicio2.2.py
@@ -11,7 +11,7 @@ ball = pygame.image.load("OBAMNA.png")
 ball = pygame.transform.scale(ball, (100, 140))
 
 # Inicializo los valores con los que se van a mover la pelota
-speed = [4,4]
+speedball = [4,4]
 
 # Pongo la pelota en el origen de coordenadas
 ballrect = ball.get_rect()
@@ -22,7 +22,7 @@ ballrect.move_ip(0,0)
 # Crea el objeto bate, y obtengo su rectángulo
 bate = pygame.image.load("nuke(barra).png")
 
-# Transforma el tamaño del objeto ball
+# Transforma el tamaño del objeto bate
 bate = pygame.transform.scale(bate, (200, 55))
 
 
@@ -31,7 +31,7 @@ baterect = bate.get_rect()
 # Pongo el bate en la parte inferior de la pantalla
 baterect.move_ip(440,600)
 
-
+batespeed = [10,0]
 
 jugando = True
 while jugando:
@@ -39,7 +39,7 @@ while jugando:
         if event.type == pygame.QUIT:
             jugando = False
 
-    # Compruebo si se ha pulsado alguna tecla
+    # Compruebo si se ha pulsado alguna tecla, y establece la velocidad del bate
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         baterect = baterect.move(-10,0)
@@ -48,25 +48,24 @@ while jugando:
 
     # Compruebo si hay colisión
     if baterect.colliderect(ballrect):
-        speed[1] = -speed[1]
+        speedball[1] = -speedball[1]
     
-   
     # Muevo la pelota
-    ballrect = ballrect.move(speed)
+    ballrect = ballrect.move(speedball)
     
      # Compruebo si la pelota llega a los límites de la ventana
     if ballrect.left < 0 or ballrect.right > ventana.get_width():
-        speed[0] = -speed[0]
+        speedball[0] = -speedball[0]
     if ballrect.top < 0 or ballrect.bottom > ventana.get_height():
-        speed[1] = -speed[1]
+        speedball[1] = -speedball[1]
+    
+    if baterect.left < 0:
+       baterect = baterect.move(10,0)
+    if baterect.right > ventana.get_width():
+        baterect = baterect.move(-10,0)
+    
     ventana.fill((238, 255, 172))
     ventana.blit(ball, ballrect)
-
-
-    
-   
-    
-    
 
     # Dibujo la pelota
     ventana.blit(ball, ballrect)
